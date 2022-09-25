37582450165c8a2868afe9ade78948e85e891b81
jimbo23 <kiefersoon@gmail.com>
2022-09-25 23:04:55 +0800

    tut 2: learning class variables

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 
 
 class Employee:
-    age = 100
+    num_of_emps = 0
+    raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = first + "." + last + "@company.com"
+        Employee.num_of_emps += 1
 
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
+
+print(Employee.num_of_emps)
 
 emp_1 = Employee("Kiefer", "Soon", 100000)
 emp_2 = Employee("Carin", "Yee", 200000)
 
-print(emp_1.fullname())
-print(Employee.fullname(emp_2))
+print(Employee.num_of_emps)
